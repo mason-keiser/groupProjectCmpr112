8d708fd7f6eae6eba4dcfca82b11e581986a13c0
Mason Keiser <mkeiser@firstam.com>
2023-10-12 00:04:58 -0700

    fixed env issue

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,8 @@
+
+H
+src/main/logger/Log.java,e\0\e067320afd25d7a6eae918a093878f568bbd970d
+B
+src/main/Main.java,f\e\fe03b4fd3afe162c6e01309b98a22588e093d274
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.logging.Level;
+
+import static main.logger.Log.LOGGER;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LOGGER.log(Level.INFO, "Hello World");
+    }
+}
