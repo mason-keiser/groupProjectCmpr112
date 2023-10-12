bc46e1f5bec906588100fcd4de320dda2beaa6a5
Mason Keiser <mkeiser@firstam.com>
2023-10-12 00:53:29 -0700

    added bill setter

--- src/main/BillManager.java
+++ src/main/BillManager.java
@@ -4,11 +4,15 @@ import static main.logger.Log.LOGGER;
 
 //Class used to manipulate user's bills and present data
 public class BillManager {
-    public static Object bills;
+    public Object bills;
 
     public static double getTotalBillCost(Object bills) {return 0;}
 
     public static void printBillList(Object bills) {}
 
     public static void deleteBill() {}
+
+    public void setBills(Object bills) {
+        this.bills = bills;
+    }
 }
